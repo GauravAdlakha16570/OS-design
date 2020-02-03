d30625e4ef3a827254be40e0acab24ca41f478ba
Gaurav Adlakha <gadla001@sledge.cs.ucr.edu>
2020-02-02 22:42:08 -0800

    began clone func

--- proc.c
+++ proc.c
@@ -532,3 +532,55 @@ procdump(void)
     cprintf("\n");
   }
 }
+
+// clone function for thread creation. Body comes from the fork function
+int
+clone(void)
+{
+  int i, pid;
+  struct proc *np;
+  struct proc *curproc = myproc();
+
+  // Allocate process.
+  if((np = allocproc()) == 0){
+    return -1;
+  }
+
+  // Copy process state from proc.
+  /*if((np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0){
+    kfree(np->kstack);
+    np->kstack = 0;
+    np->state = UNUSED;
+    return -1;
+  }
+   */
+  np->sz = curproc->sz;
+  np->parent = curproc;
+  *np->tf = *curproc->tf;
+  np->pgdir = curproc->pgdir; //threads share the same page table
+
+  // Clear %eax so that fork returns 0 in the child.
+  np->tf->eax = 0;
+
+  for(i = 0; i < NOFILE; i++)
+    if(curproc->ofile[i])
+      np->ofile[i] = filedup(curproc->ofile[i]);
+  np->cwd = idup(curproc->cwd);
+
+  safestrcpy(np->name, curproc->name, sizeof(curproc->name));
+   
+// NEED TO FIGURE OUT WHAT TO DO ABOUT STACK CHANGES
+   
+   
+  pid = np->pid;
+
+  acquire(&ptable.lock);
+
+  np->state = RUNNABLE;
+
+  release(&ptable.lock);
+
+  return pid;
+}
+
+
