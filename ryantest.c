5c8c6aa2023a522e7cba3304043c8e79c13c610a
Ryan <rwils006@ucr.edu>
2020-03-02 22:34:41 -0800

    join function is working correctly and ryantest is used to test it

---
+++ ryantest.c
@@ -0,0 +1,77 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+ 
+int num_threads = 3;
+
+ 
+
+ void *testfunc(void *arg) {
+
+     int num = *(int*)arg;
+	 
+     if (num == 0){
+         sleep(1000);
+     }
+     else if (num == 1){
+         sleep(100);
+     }
+     else if (num == 2){
+         sleep(500);
+     }
+
+     
+     printf(1, "Thread %d exitting\n", num);
+
+	 exit();
+ }
+ 
+ 
+ 
+int
+main(void)
+{
+	void *stacks[num_threads];
+    int *args[num_threads];
+
+    for (int i = 0; i < num_threads; i++) {
+        stacks[i] = (void*)malloc(4096); //allocate a stack for each thread
+        if (!stacks[i]) {
+            printf(1, "main: could not allocate stacks");
+            exit();
+        }
+
+        args[i] = (int*)malloc(4); //each argument gets a spot in memory
+        if (!args[i]) {
+            printf(1, "main: could not allocate args");
+            exit();
+        }
+
+        *args[i] = i;
+    }
+
+    printf(1, "main: running with %d threads.\n", num_threads);
+
+    //Time to make the threads
+    int tid = 0;
+
+    for (int i = 0; i < num_threads; i++) {
+        tid = clone(&testfunc, stacks[i], args[i]);
+        //sleep(100);
+        printf(1, "main: created thread with tid: %d\n", tid);
+    }
+
+    //Time to wait for all of them to finish
+    
+
+    for (int i = 0; i < num_threads; i++) {
+        printf(1, "starting join\n");
+        tid = join();
+        printf(1, "thread %d joined\n", tid);
+    }
+
+    printf(1, "main: all threads joined\n");
+	
+
+  exit();
+}
